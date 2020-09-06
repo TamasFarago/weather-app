bba9439338dad27af331d60ed1e6d47294061424
Tamas Farago <tamas@Tamass-MacBook-Pro.local>
2020-09-07 00:57:29 +0200

    separate search bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react'
 import Class from "./Class"
+import SearchBar from "./SearchBar"
+
+const api = {
+  key: "cd136a1ec143e3bd9413139ceeb924c4",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {query: "", weather: ""}
+    this.handleChange = this.handleChange.bind(this)
+
+  }
+
+  search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${this.state.query}&units=metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(result => {
+        this.setState({query: "", weather: result})
+        
+        console.log(result)
+      })
+    }
+  }
+
+  handleChange(e){
+    this.setState({query: e.target.value})
+}
+
   render() {
     return (
       <div className="site">
-       
-        <Class />
-      </div>
+        <div className="search-box">
+         <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={this.handleChange}
+            value={this.state.query}
+            onKeyPress={this.search}></input>
+        </div>
+        <Class 
+          weather={this.state.weather}
+        />
+      </div> 
     )
   }
 }
--- src/Class.js
+++ src/Class.js
@@ -1,31 +1,17 @@
 import React, { Component } from 'react'
 import './App.css';
 
-const api = {
-  key: "cd136a1ec143e3bd9413139ceeb924c4",
-  base: "https://api.openweathermap.org/data/2.5/"
-}
+
 
 export default class Class extends Component {
     constructor(props){
         super(props);
-        this.state = {query: "", weather: ""}
-        this.search = this.search.bind(this)
+        
         this.dateBuilder = this.dateBuilder.bind(this)
         this.handleChange = this.handleChange.bind(this)
     }
 
-    search = evt => {
-        if (evt.key === "Enter") {
-          fetch(`${api.base}weather?q=${this.state.query}&units=metric&APPID=${api.key}`)
-          .then(res => res.json())
-          .then(result => {
-            this.setState({query: "", weather: result})
-            
-            console.log(result)
-          })
-        }
-      }
+   
 
       dateBuilder = (d) => {
         let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
@@ -46,31 +32,23 @@ export default class Class extends Component {
 
     render() {
         return (
-            <div className={(typeof this.state.weather.main != "undefined") ? ((this.state.weather.main.temp > 16) ? "App warm"
+            <div className={(typeof this.props.weather.main != "undefined") ? ((this.props.weather.main.temp > 16) ? "App warm"
     : "App cold") : "App cold"}>
       
       <main>
-        <div className="search-box">
-         <input
-            type="text"
-            className="search-bar"
-            placeholder="Search..."
-            onChange={this.handleChange}
-            value={this.state.query}
-            onKeyPress={this.search}></input>
-        </div>
-        {(typeof this.state.weather.main != "undefined") ? (
+        
+        {(typeof this.props.weather.main != "undefined") ? (
           <div>
             <div className="location-box">
-              <div className="location">{this.state.weather.name}, {this.state.weather.sys.country}</div>
+              <div className="location">{this.props.weather.name}, {this.props.weather.sys.country}</div>
               <div className="date">{this.dateBuilder(new Date())}</div>
             </div>
             <div className="weather-box">
               <div className="temp">
-                {Math.round(this.state.weather.main.temp)}°c
+                {Math.round(this.props.weather.main.temp)}°c
               </div>
               <div className="weather">
-                {this.state.weather.weather[0].main}
+                {this.props.weather.weather[0].main}
               </div>
               
             </div>
